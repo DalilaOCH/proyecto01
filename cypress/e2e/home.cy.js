569e9a98e01ef73b33944555f6fce3ecf2a4cd6c
Dalila <dalila.ochaves@gmail.com>
2023-06-23 17:48:24 -0600

    tarea1

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,13 +1,47 @@
-describe('template spec', () => {
+/*describe('template spec', () => {
   it('frist test', () => {
     cy.visit('http://localhost:3000')
-  })
+  })*/
 
-  it('second test', () => {
+  /*it('second test', () => {
     cy.visit('https://example.cypress.io')
+  })*/
+
+  /*it('third test', () => {
+    //cy.visit('http://localhost:3000')
+  })*/
+
+  /*it('xpath test', () => {
+    cy.visit('https://example.cypress.io')
+    cy.xpath('//h1').contains("Kitchen Sink")
+    cy.xpath('//h1').should('have.length', 1);
+  });*/
+
+
+  /*it.only("the features on the homepage are correct", () => {
+    cy.visit('http://localhost:3000')
+    cy.get("dt")
+  })*/
+
+  it.only("Prueba1", () => {
+    cy.visit('http://localhost:3000')
+    cy.xpath('//h1').contains("Cypress")
   })
 
-  it('third test', () => {
+  it.only("Prueba2", () => {
     cy.visit('http://localhost:3000')
+    cy.xpath('//p').invoke('text')
+    .should('match', /^Lorem ipsum dolor sit, amet consectetur adipisicing elit./)
   })
-})
+
+  it.only("Prueba3", () => {
+    cy.visit('http://localhost:3000')
+    cy.get("dt").should('have.length', 2)
+  })
+  
+  
+
+
+
+
+
